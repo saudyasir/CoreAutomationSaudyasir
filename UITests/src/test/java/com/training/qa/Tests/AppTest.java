fb71f820e25206f2650a5265e95c39135304e576
Saud <Saud@DESKTOP-H1PTRJQ>
2024-08-29 13:42:08 +0530

    1342

--- UITests/src/test/java/com/training/qa/Tests/AppTest.java
+++ UITests/src/test/java/com/training/qa/Tests/AppTest.java
@@ -5,38 +5,15 @@ import org.testng.annotations.AfterTest;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.testng.*;
+import org.testng.TestNG;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-	
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-   // @AfterTest
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	//SoftAssert ad = new SoftAssert();
 }
